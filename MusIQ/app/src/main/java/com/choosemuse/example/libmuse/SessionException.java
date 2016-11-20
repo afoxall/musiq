d30f3428f487a440523c3383fdb77bc156b470af
Alisha Foxall <13amf7@queensu.ca>
2016-11-19 20:33:16 -0500

    deleted oldmusiq, completed workSession class

---
+++ MusIQ/app/src/main/java/com/choosemuse/example/libmuse/SessionException.java
@@ -0,0 +1,13 @@
+package com.musiq;
+
+/**
+ * Created by User on 2016-11-19.
+ */
+
+public class SessionException extends Exception{
+
+    SessionException(String msg){
+        super(msg);
+    }
+
+}
