43b6f5dbca0ee43215adef14079e564a875483b8
almagaro <almagaro94@hotmail.com>
2016-11-20 06:57:04 -0500

    Add files via upload

---
+++ MusIQ/app/src/main/java/com/choosemuse/example/libmuse/NewSessionActivity.java
@@ -0,0 +1,80 @@
+package com.choosemuse.example.libmuse;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**
+ * Created by User on 2016-11-19.
+ */
+
+
+public class NewSessionActivity extends Activity{
+
+    Button btnReset;
+    Button btnCreateSession;
+
+    EditText n = (EditText)findViewById(R.id.sessionName);
+    String name= n.getText().toString();
+    EditText rest = (EditText) findViewById(R.id.restLength) ;
+    int restValue = Integer.parseInt( rest.getText().toString() );
+    EditText work = (EditText) findViewById(R.id.workLength) ;
+    int workValue = Integer.parseInt( work.getText().toString() );
+    EditText numC = (EditText) findViewById(R.id.numCycles) ;
+    final int numCValue = Integer.parseInt( numC.getText().toString() );
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_session);
+
+        btnReset = (Button)findViewById(R.id.resetbtn);
+        btnCreateSession = (Button)findViewById(R.id.createSessionbtn);
+
+
+        btnReset.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                n.setText(null);
+                rest.setText(null);
+                work.setText(null);
+                numC.setText(null);
+            }
+        });
+
+        btnCreateSession.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Create WorkSessionTemplate
+                try {
+                    addSessionTemplate(NewSessionActivity.this,name, workValue, restValue, numCValue);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                //return to front page
+                Intent i = new Intent(NewSessionActivity.this, MainActivity.class );
+                startActivity(i);
+            }
+        });
+    }
+    // Add session template obj
+    static void addSessionTemplate(Context ctx, String n, int work, int rest, int num) throws IOException {
+
+        WorkSessionTemplate workSes = new WorkSessionTemplate(n,work,rest,num);
+        File dir = ctx.getFilesDir(); // get app directory
+        String path = dir.getAbsolutePath() + "session_templates.ser";
+        FileOutputStream stream = new FileOutputStream(path, false);
+        ObjectOutputStream objStream = new ObjectOutputStream(stream);
+        objStream.writeObject(workSes);
+    }
+}
+
+
